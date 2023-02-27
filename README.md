c2dc1c62607beec8ff5c4f81c9930a3aa82613c8
Nai <nailahmukhtar@Nailahs-Air.home>
2023-02-27 21:03:02 +0000

    added questions to questions array and called in promptUser function

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# readme-generator
+# Nailah's Project
+
+  ## Description
+  
+  It's a super cool project
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  npm i
+  
+  ## Usage
+  
+  do some cool stuff
+  
+  ## Credits
+  
+  undefined
+  
+  ## License
+  
+  MIT
+  ---
+  ## Badges
+  
+  ![license](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  ## Tests
+  
+  blah blah blah tests run
+
--- index.js
+++ index.js
@@ -1,30 +1,74 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const util = require('util');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project?',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'Installation steps?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use the application?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Command to run tests?',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How can someone contribute to this project?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'License type?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Github username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Contact Information (Email)?',
+  },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () => {
+  return inquirer.prompt(questions)
+};
 
 // function to initialize program
 const init = async () => {
     console.log('hi');
     try {
       const answers = await promptUser();
-  
-      const html = generateMarkdown(data);
+      const md = generateMarkdown(answers);
   
       await writeFileAsync('README.md', md);
   
-      console.log('Successfully wrote to readme.md');
+      console.log('Successfully wrote to a readme.md file');
     } catch (err) {
       console.log(err);
     }
